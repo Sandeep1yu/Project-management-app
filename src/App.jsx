f8bd8f1816ba34539ede84c72535f560f81f317c
Sandeep <sandeep167kumar@gmail.com>
2023-11-06 18:56:14 +0530

    deleted the comment part in the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,51 @@
-import { useState } from 'react'
-import './App.css'
-import NewProject from './components/NewProject'
-import NoProjectSelect from './components/NoProjectSelect.jsx'
-import ProjectsSidebar from './components/ProjectsSidebar.jsx'
-
+import { useState } from "react";
+import "./App.css";
+import NewProject from "./components/NewProject";
+import NoProjectSelect from "./components/NoProjectSelect.jsx";
+import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
 
 function App() {
   const [selectedState, setSelectedState] = useState({
     selectedItem: undefined,
-    projects: []
-  })
+    projects: [],
+  });
 
-  const addProjectItem = ()=> {
-    setSelectedState(prevState=>{
+  const addProjectItem = () => {
+    setSelectedState((prevState) => {
       return {
         ...prevState,
-        selectedItem: null
+        selectedItem: null,
+      };
+    });
+  };
 
-      }
-    })
-  }
-
-  const handleAddProject = (projectData)=> {
-    setSelectedState(prevState =>{
+  const handleAddProject = (projectData) => {
+    setSelectedState((prevState) => {
       const newProject = {
         ...projectData,
-        id: Math.random()
-      }
+        id: Math.random(),
+      };
       return {
         ...prevState,
-        projects: [...selectedState.projects, newProject]
-      }
-    })
-  }
+        projects: [...selectedState.projects, newProject],
+      };
+    });
+  };
 
   console.log(selectedState);
   let content;
-  if(selectedState.selectedItem === null) {
-    content = <NewProject onAdd={handleAddProject}/>
-  }
-  else if(selectedState.selectedItem === undefined) {
-    content = <NoProjectSelect onStartProject={addProjectItem}/>
+  if (selectedState.selectedItem === null) {
+    content = <NewProject onAdd={handleAddProject} />;
+  } else if (selectedState.selectedItem === undefined) {
+    content = <NoProjectSelect onStartProject={addProjectItem} />;
   }
 
   return (
-    
-      <main className="main">
-        <ProjectsSidebar onStartProject={addProjectItem}/>
-        {/* <NewProject /> */}
-        {/* <NoProjectSelect onStartProject={addProjectItem}/> */}
-        {content}
-      </main>
-      
-    
-  )
+    <main className="main">
+      <ProjectsSidebar onStartProject={addProjectItem} />
+      {content}
+    </main>
+  );
 }
 
-export default App
+export default App;
